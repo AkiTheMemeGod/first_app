94c81ee2707e528b00520af39bb34b3909b00ff4
AkiTheMemeGod <142716100+AkiTheMemeGod@users.noreply.github.com>
2023-09-02 09:34:35 +0530

    basic changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello World ! ")));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 9, 206, 232),
+        body: Center(
+          child: Text("Hello World ! "),
+        ),
+      ),
+    ),
+  );
 }
