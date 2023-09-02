a1c82008793fe32120ae2b3f59670ce7a58e3430
AkiTheMemeGod <142716100+AkiTheMemeGod@users.noreply.github.com>
2023-09-02 20:31:50 +0530

    added gradients

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 9, 206, 232),
-        body: Center(
-          child: Text("Hello World ! "),
+        //backgroundColor: Color.fromARGB(255, 19, 75, 178),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 19, 75, 178),
+              Color.fromARGB(255, 95, 22, 196),
+              Color.fromARGB(255, 149, 79, 230)
+            ]),
+          ),
+          child: const Center(
+            child: Text("This a hjomne screen with a cool gradient ! "),
+          ),
         ),
       ),
     ),
